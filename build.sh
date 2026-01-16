2e0f45f1afe1400c589e38b08d369cd3314fb7af
PuPoKuangChih <175012926+PuPoKuangChih@users.noreply.github.com>
2026-01-16 19:43:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,53 +1,36 @@
 #!/bin/bash
+set -e
+
 mkdir -p armbian
 
-# 读取环境变量 (带默认值)
-VERSION_TYPE="${VERSION_TYPE:-standard}"
-if [ "$VERSION_TYPE" = "debian12_minimal" ]; then
-  echo "构建debian12_minimal-armbian..."
-  FILE_NAME="Armbian_25.11.1_Uefi-x86_noble_current_6.12.58_minimal.img.xz"
-elif [ "$VERSION_TYPE" = "ubuntu24_minimal" ]; then
-  echo "构建ubuntu24_minimal-armbian..." 
-  FILE_NAME="Armbian_25.11.1_Uefi-x86_noble_current_6.12.58_minimal.img.xz"
-elif [ "$VERSION_TYPE" = "homeassistant_debian12_minimal" ]; then
-  echo "构建homeassistant全家桶版armbian..." 
-  FILE_NAME="Armbian_25.11.1_Uefi-x86_noble_current_6.12.58_minimal.img.xz"
-else 
-  echo "构建standard-armbian..."
-  FILE_NAME="Armbian_25.11.1_Uefi-x86_noble_current_6.12.58_minimal.img.xz"
-fi
+BASE_OS="${BASE_OS:-trixie}"   # noble | trixie
+VERSION="25.11.1"
+KERNEL="6.12.58"
 
-REPO="wukongdaily/img-installer"
-TAG="2025-03-12"
+FILE_NAME="Armbian_${VERSION}_Uefi-x86_${BASE_OS}_current_${KERNEL}_minimal.img.xz"
+BASE_URL="https://mirror.twds.com.tw/armbian-dl/uefi-x86/archive"
+DOWNLOAD_URL="${BASE_URL}/${FILE_NAME}"
 OUTPUT_PATH="armbian/armbian.img.xz"
 
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/$REPO/releases/tags/$TAG | jq -r '.assets[] | select(.name == "'"$FILE_NAME"'") | .browser_download_url')
+echo "Base OS      : $BASE_OS"
+echo "Downloading  : $DOWNLOAD_URL"
 
-if [[ -z "$DOWNLOAD_URL" ]]; then
-  echo "错误：未找到文件 $FILE_NAME"
-  exit 1
-fi
+curl -fL -o "$OUTPUT_PATH" "$DOWNLOAD_URL"
 
-echo "下载地址: $DOWNLOAD_URL"
-echo "下载文件: $FILE_NAME -> $OUTPUT_PATH"
-curl -L -o "$OUTPUT_PATH" "$DOWNLOAD_URL"
+echo "下载成功，检查文件："
+file "$OUTPUT_PATH"
 
-if [[ $? -eq 0 ]]; then
-  echo "下载armbian成功!"
-  file armbian/armbian.img.xz
-  echo "正在解压为:armbian.img"
-  xz -d armbian/armbian.img.xz
-  ls -lh armbian/
-  echo "准备合成 armbian 安装器"
-else
-  echo "下载失败！"
-  exit 1
-fi
+echo "解压 armbian.img"
+xz -d "$OUTPUT_PATH"
+
+ls -lh armbian/
+
+echo "准备合成 armbian 安装器"
 
 mkdir -p output
 docker run --privileged --rm \
-        -v $(pwd)/output:/output \
-        -v $(pwd)/supportFiles:/supportFiles:ro \
-        -v $(pwd)/armbian/armbian.img:/mnt/armbian.img \
-        debian:buster \
-        /supportFiles/build.sh
+  -v "$(pwd)/output:/output" \
+  -v "$(pwd)/supportFiles:/supportFiles:ro" \
+  -v "$(pwd)/armbian/armbian.img:/mnt/armbian.img" \
+  debian:buster \
+  /supportFiles/build.sh
